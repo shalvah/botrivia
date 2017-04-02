2be11972738cd03ed1a863e1417fb99163b66cc0
Shalvah <diakon.ng@gmail.com>
2017-04-02 23:45:50 +0100

    Fix data error

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,7 +15,6 @@ class MainController extends Controller
     public function receive(Request $request)
     {
         $data = $request->all();
-
         //get the user id and reply
         $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
         $this->sendTextMessage($id, "Hello");
@@ -39,6 +38,7 @@ class MainController extends Controller
         $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env("PAGE_ACCESS_TOKEN"));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageData));
         Log::info(print_r(curl_exec($ch), true));
