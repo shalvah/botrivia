61fc994318171a6c93a965854641026477033e80
Shalvah <diakon.ng@gmail.com>
2017-04-02 20:36:48 +0100

    Fix query param bug

--- app/Http/Middleware/VerifyMiddleware.php
+++ app/Http/Middleware/VerifyMiddleware.php
@@ -15,10 +15,9 @@ class VerifyMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $data = $request->all();
-        if ($data["hub.mode"] === "subscribe"
-            && $data["hub.verify_token"] === env("MESSENGER_VERIFY_TOKEN")) {
-            return response($data["hub.challenge"], 200);
+        if ($request->input("hub_mode") === "subscribe"
+            && $request->input("hub_verify_token") === env("MESSENGER_VERIFY_TOKEN")) {
+            return response($request->input("hub_challenge"), 200);
         }
         return $next($request);
     }
