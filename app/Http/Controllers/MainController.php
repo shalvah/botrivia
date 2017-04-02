9973dabeab1edff877b191f83afb9f68c3d2ec9c
Shalvah <diakon.ng@gmail.com>
2017-04-02 22:49:29 +0100

    Update test response

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,6 +8,31 @@ class MainController extends Controller
 {
     public function receive(Request $request)
     {
-        return response("Hello");
+        $data = $request->all();
+
+        //get the user id and reply
+        $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
+        $this->sendTextMessage($id, "Hello");
+    }
+
+    private function sendTextMessage($recipientId, $messageText)
+    {
+        $messageData = [
+            "recipient" => [
+                "id" => $recipientId
+            ],
+            "message" => [
+                "text" => $messageText
+            ]
+        ];
+        $this->callSendApi($messageData);
+    }
+
+    private function callSendApi($messageData)
+    {
+        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env("PAGE_ACCESS_TOKEN"));
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageData));
+        curl_exec($ch);
     }
 }
