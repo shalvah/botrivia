c8779ea1015a640e90388df344c7880a4a7479e9
Shalvah <diakon.ng@gmail.com>
2017-04-18 05:54:21 +0100

    Fix DI issue

--- app/Bot/Webhook/Messaging.php
+++ app/Bot/Webhook/Messaging.php
@@ -13,7 +13,7 @@ class Messaging
     private $message;
     private $type;
 
-    public function __construct(array $data)
+    public function __construct($data)
     {
         $this->senderId = $data["sender"]["id"];
         $this->recipientId = $data["recipient"]["id"];
