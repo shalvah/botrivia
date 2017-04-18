34c652f52e7ba7c75500ab54f48619ed9d6510e7
Shalvah <diakon.ng@gmail.com>
2017-04-18 03:56:22 +0100

    Add Bot class

---
+++ app/Bot/Bot.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Bot;
+
+
+use App\Bot\Webhook\Messaging;
+use Illuminate\Support\Facades\Log;
+
+class Bot
+{
+    private $messaging;
+
+    public function __construct(Messaging $messaging)
+    {
+        $this->messaging = $messaging;
+    }
+
+    public function extractDataFromMessage()
+    {
+        $matches = [];
+        //single letter message means an answer
+        if (preg_match("/^(\\w)\$/i", $this->messaging->text, $matches)) {
+            return [
+                "type" => Trivia::$ANSWER,
+                "data" => [
+                    "answer" => $matches[0]
+                ]
+            ];
+        } else if (preg_match("/^new|next\$/i", $this->messaging->text, $matches)) {
+            return [
+                "type" => Trivia::$NEW_QUESTION,
+                "data" => []
+            ];
+        }
+        return [
+            "type" => "unknown",
+            "data" => []
+        ];
+    }
+
+    public function reply($data)
+    {
+        if (method_exists($data, "toMessage")) {
+            $data = $data->toMessage();
+        } else if (typeOf($data) == "string") {
+            $data = ["text" => $data];
+        }
+        $id = $this->messaging->senderId;
+        $this->sendMessage($id, $data);
+    }
+
+    private function sendMessage($recipientId, $message)
+    {
+        $messageData = [
+            "recipient" => [
+                "id" => $recipientId
+            ],
+            "message" => $message
+        ];
+        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env("PAGE_ACCESS_TOKEN"));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageData));
+        Log::info(print_r(curl_exec($ch), true));
+    }
+}
