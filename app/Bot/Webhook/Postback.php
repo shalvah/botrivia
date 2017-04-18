ae7d756c287c5a4d3ba9db02d86794c771990e6a
Shalvah <diakon.ng@gmail.com>
2017-04-18 12:53:00 +0100

    Setup postback class

--- app/Bot/Webhook/Postback.php
+++ app/Bot/Webhook/Postback.php
@@ -5,5 +5,29 @@ namespace App\Bot\Webhook;
 
 class Postback
 {
+    private $payload;
+    private $referral;
+
+    public function __construct(array $data)
+    {
+        $this->payload = $data["payload"];
+        $this->referral = isset($data["referral"]) ? $data["referral"] : [];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayload()
+    {
+        return $this->payload;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReferral()
+    {
+        return $this->referral;
+    }
 
 }
