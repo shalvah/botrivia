327429b346cc6572b0cece75ae493205e7006a5f
Shalvah <diakon.ng@gmail.com>
2017-04-18 06:11:28 +0100

    Add logging for debugging

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Bot\Webhook\Entry;
 use App\Jobs\BotHandler;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MainController extends Controller
 {
     public function receive(Request $request)
     {
         $entries = Entry::getEntries($request);
-        foreach ($entries as $entry) {
+        Log::info(print_r($entries, true));
+        //foreach ($entries as $entry) {
             $messagings = $entry->getMessagings();
             foreach ($messagings as $messaging) {
                 dispatch(new BotHandler($messaging));
