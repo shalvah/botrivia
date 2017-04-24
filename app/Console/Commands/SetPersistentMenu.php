09010bcd03d98fa581b4985f103dce0fc594f38c
Shalvah <diakon.ng@gmail.com>
2017-04-24 04:13:37 +0100

    Add persistent menu creation command

---
+++ app/Console/Commands/SetPersistentMenu.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SetPersistentMenu extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bot:menu:set {--locale=default}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create the persistent menu for the bot';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $data = [
+            "persistent_menu" => [
+                [
+                    "locale" => $this->option("locale"),
+                    "call_to_actions" => [
+                        [
+                            "title" => "View source code",
+                            "type" => "web_url",
+                            "url" => "http://github.com/shalvah/botrivia",
+                            "webview_height_ratio" => "full"
+                        ],
+                        [
+                            "title" => "How to build this bot",
+                            "type" => "web_url",
+                            "url" => "https://tutorials.botsfloor.com/building-a-facebook-messenger-trivia-bot-with-laravel-part-1-61209b0e35db",
+                            "webview_height_ratio" => "full"
+                        ],
+                    ]
+                ]
+            ]
+        ];
+        $ch = curl_init('https://graph.facebook.com/v2.6/me/messenger_profile?access_token=' . env("PAGE_ACCESS_TOKEN"));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+        $result = json_decode(curl_exec($ch), true);
+        $this->info(print_r($result));
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\SetGreetingText::class,
         Commands\SetGetStartedButton::class,
+        Commands\SetPersistentMenu::class,
     ];
 
     /**
